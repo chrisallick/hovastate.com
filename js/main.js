297fd2626fb7c0ba33a77dc7ad7dcf989dc4183a
Chris Allick <chris_allick@gspsf.com>
2013-09-25 17:50:59 -0700

    animate on click log

--- js/main.js
+++ js/main.js
@@ -4,4 +4,12 @@ $(document).ready(function() {
 	gl = new GifLoader();
 
 	gl.load( $(".gif")[0], "./img/logo.gif" );
+
+	$("#logo").click(function(event){
+		event.preventDefault();
+
+		$("html,body").animate({
+			scrollTop: $(document).height()
+		})
+	});
 });
